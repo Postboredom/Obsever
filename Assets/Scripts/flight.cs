a927c8d4652d6dc49f0501786f10817d90c2848c
Postboredom <noah1010@me.com>
2019-01-31 21:20:33 -0800

    Base Game
    the base game we use to create the observer patteren

---
+++ Assets/Scripts/flight.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flight : MonoBehaviour {
+
+    public GameObject Player;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+        if(Input.GetButtonDown("Jump"))
+        {
+            Player.GetComponent<Rigidbody2D>().AddForce(Vector2.up * 220);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            Player.GetComponent<Rigidbody2D>().AddForce(Vector2.left * 10);
+            Player.GetComponent<SpriteRenderer>().flipX = false;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            Player.GetComponent<Rigidbody2D>().AddForce(Vector2.right * 10);
+            Player.GetComponent<SpriteRenderer>().flipX = true;
+        }
+    }
+}
