cbf946f8b9c269c8d7c9c0b8319e91825bba20f5
Postboredom <noah1010@me.com>
2019-03-15 17:01:01 -0700

    quick push

---
+++ Assets/Scripts/CharacterData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+[Serializable]
+
+public class CharacterData {
+    public string charname;
+    public string power;
+    public short charge;
+
+    public void setall(CharacterData data)
+    {
+        charname = data.charname;
+        power = data.power;
+        charge = data.charge;
+    }
+}
